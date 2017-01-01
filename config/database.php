06ea891aeed0840fd45972d9bd6a9caad45998c1
kavan <kavanpancholi@gmail.com>
2017-01-02 00:59:08 +0530

    Add heroku db creds fix

--- config/database.php
+++ config/database.php
@@ -62,11 +62,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', $host),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', $database),
-            'username' => env('DB_USERNAME', $database),
-            'password' => env('DB_PASSWORD', $password),
+            'host'      => $host,
+            'database'  => $database,
+            'username'  => $username,
+            'password'  => $password,
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
